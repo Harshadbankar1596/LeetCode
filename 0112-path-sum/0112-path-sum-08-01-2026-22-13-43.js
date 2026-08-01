96c8c5eaac8cc5d94be135b54efc395571de9938
Harshad Bankar <harshadbankar1596@gmail.com>
2026-08-01 22:13:45 +0530

    Time: 0 ms (100%), Space: 58.5 MB (36.74%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum-08-01-2026-22-13-43.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+    if (!root) return false;
+    function add(a, b) {
+        return a + b
+    }
+
+    let ans = false
+
+    function traverse(root, sum) {
+        if (!root) return;
+        let newSum = add(sum, root.val)
+        if (!root.left && !root.right) {
+            if (newSum === targetSum) {
+                ans = ans || true
+            }
+        }
+        root.left && traverse(root.left, newSum)
+        root.right && traverse(root.right, newSum)
+    }
+
+    traverse(root, 0)
+    return ans
+
+};
