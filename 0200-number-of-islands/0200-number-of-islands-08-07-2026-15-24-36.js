9ef2e487d682d826a1ff0ad037d93affc4aed5ff
Harshad Bankar <harshadbankar1596@gmail.com>
2026-08-07 15:24:38 +0530

    Time: 80 ms (20.72%), Space: 61.6 MB (35.51%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands-08-07-2026-15-24-36.js
@@ -0,0 +1,97 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (arr) {
+    if (!arr) return 0;
+    let m = arr.length
+    let n = arr[0].length
+
+    // let stack = [];
+
+    // for (let i = 0; i < m; i++) {
+    //     const row = arr[i];
+
+    //     for (let j = 0; j < n; j++) {
+    //         if (row[j] === 1) {
+    //             stack.push([i, j]);
+    //         }
+    //     }
+    // }
+
+
+    let count = 0
+    let temp = []
+
+    for (let x = 0; x < m; x++) {
+        for (let y = 0; y < n; y++) {
+            if (arr[x][y] == 0) {
+                continue;
+            }
+
+            temp.push([x, y])
+
+            while (temp.length) {
+                let [i, j] = temp.shift()
+
+                arr[i][j] = 0;
+
+                if (i - 1 >= 0 && arr[i - 1][j] == 1) {
+                    temp.push([i - 1, j])
+                    arr[i - 1][j] = 0
+                }
+                if (i + 1 < m && arr[i + 1][j] == 1) {
+                    temp.push([i + 1, j])
+                    arr[i + 1][j] = 0
+                }
+                if (j - 1 >= 0 && arr[i][j - 1] == 1) {
+                    temp.push([i, j - 1])
+                    arr[i][j - 1] = 0
+                }
+                if (j + 1 < n && arr[i][j + 1] == 1) {
+                    temp.push([i, j + 1])
+                    arr[i][j + 1] = 0
+                }
+            }
+
+            count++
+        }
+    }
+
+    // while (stack.length) {
+    //     let [x, y] = stack.shift()
+    //     if (arr[x][y] == 0) {
+    //         continue;
+    //     }
+
+    //     temp.push([x, y])
+
+    //     while (temp.length) {
+    //         let [i, j] = temp.shift()
+
+    //         arr[i][j] = 0;
+
+    //         if (i - 1 >= 0 && arr[i - 1][j] == 1) {
+    //             temp.push([i - 1, j])
+    //             arr[i - 1][j] = 0
+    //         }
+    //         if (i + 1 < m && arr[i + 1][j] == 1) {
+    //             temp.push([i + 1, j])
+    //             arr[i + 1][j] = 0
+    //         }
+    //         if (j - 1 >= 0 && arr[i][j - 1] == 1) {
+    //             temp.push([i, j - 1])
+    //             arr[i][j - 1] = 0
+    //         }
+    //         if (j + 1 < n && arr[i][j + 1] == 1) {
+    //             temp.push([i, j + 1])
+    //             arr[i][j + 1] = 0
+    //         }
+    //     }
+
+    //     count++
+
+    // }
+
+    return count
+};
