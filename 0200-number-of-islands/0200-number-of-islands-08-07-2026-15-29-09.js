ee26173e58ee93647cae543420cef2ac069b8d13
Harshad Bankar <harshadbankar1596@gmail.com>
2026-08-07 15:29:11 +0530

    Time: 75 ms (25.55%), Space: 65.7 MB (19.92%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands-08-07-2026-15-29-09.js
@@ -0,0 +1,48 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function (arr) {
+    if (!arr) return 0;
+    let m = arr.length
+    let n = arr[0].length
+    let count = 0
+    let temp = []
+
+    for (let x = 0; x < m; x++) {
+        for (let y = 0; y < n; y++) {
+            if (arr[x][y] == 0) {
+                continue;
+            }
+
+            temp.push([x, y])
+
+            while (temp.length) {
+                let [i, j] = temp.pop()
+
+                arr[i][j] = 0;
+
+                if (i - 1 >= 0 && arr[i - 1][j] == 1) {
+                    temp.push([i - 1, j])
+                    arr[i - 1][j] = 0
+                }
+                if (i + 1 < m && arr[i + 1][j] == 1) {
+                    temp.push([i + 1, j])
+                    arr[i + 1][j] = 0
+                }
+                if (j - 1 >= 0 && arr[i][j - 1] == 1) {
+                    temp.push([i, j - 1])
+                    arr[i][j - 1] = 0
+                }
+                if (j + 1 < n && arr[i][j + 1] == 1) {
+                    temp.push([i, j + 1])
+                    arr[i][j + 1] = 0
+                }
+            }
+
+            count++
+        }
+    }
+
+    return count
+};
