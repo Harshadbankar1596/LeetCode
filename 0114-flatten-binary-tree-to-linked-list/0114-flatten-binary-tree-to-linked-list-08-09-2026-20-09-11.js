c09f3909eb9ffd2572f740a736f775059e82fad2
Harshad Bankar <harshadbankar1596@gmail.com>
2026-08-09 20:09:13 +0530

    Time: 19 ms (1.35%), Space: 62.1 MB (7.42%) - LeetHub

---
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list-08-09-2026-20-09-11.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {void} Do not return anything, modify root in-place instead.
+ */
+var flatten = function (root) {
+    if (!root) return [];
+    let temp = []
+    function traverse(root) {
+        if (!root) return;
+        temp.push(root)
+        traverse(root.left)
+        traverse(root.right)
+    }
+    traverse(root)
+    console.log("temp => ", temp)
+
+    let current = root
+    for (let i = 0; i < temp.length - 1; i++) {
+        temp[i].left = null;
+        temp[i].right = temp[i + 1];
+    }
+    temp[temp.length - 1].left = null;
+    temp[temp.length - 1].right = null;
+};
