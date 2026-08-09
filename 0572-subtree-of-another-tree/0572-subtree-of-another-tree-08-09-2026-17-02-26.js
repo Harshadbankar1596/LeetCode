572ce7c0100b9797d6ef661ed1daa7b47fb9f685
Harshad Bankar <harshadbankar1596@gmail.com>
2026-08-09 17:02:28 +0530

    Time: 5 ms (79.52%), Space: 62.5 MB (49.66%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree-08-09-2026-17-02-26.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(groot, subRoot) {
+
+    function checkSame(root1 , root2){
+        if(!root1 && !root2) return true;
+        if(!root1 || !root2) return false;
+        
+        return (root1.val == root2.val && checkSame(root1.left , root2.left) && checkSame(root1.right , root2.right))
+    }
+
+    let ans = false;
+
+    function traverse(root){
+        if(!root) return;
+
+        if(root.val === subRoot.val){
+            ans = ans || checkSame(root , subRoot)
+        }
+
+        root.left && traverse(root.left)
+        root.right && traverse(root.right)
+    }
+
+    traverse(groot)
+
+    return ans
+};
