8202ff7685b3282def3eb22c33780fadb7e6720c
Harshad Bankar <harshadbankar1596@gmail.com>
2026-08-11 21:25:36 +0530

    Time: 1 ms (45.65%), Space: 55.8 MB (34.78%) - LeetHub

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum-08-11-2026-21-25-32.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingInteger = function(nums) {
+    let set = new Set(nums)
+
+    let sum = nums[0]
+    let i = 1;
+    while(nums[i] === nums[i-1] + 1){
+        sum += nums[i]
+        i++
+    }
+
+    while(set.has(sum)){
+        sum++
+    }
+
+    return sum
+};
