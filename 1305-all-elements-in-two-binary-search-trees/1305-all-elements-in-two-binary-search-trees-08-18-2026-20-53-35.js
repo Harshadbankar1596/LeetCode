271856a64e1054d6b74f4ac32dcec2622be677ad
Harshad Bankar <harshadbankar1596@gmail.com>
2026-08-18 20:53:48 +0530

    Time: 37 ms (6.12%), Space: 72 MB (6.12%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees-08-18-2026-20-53-35.js
@@ -0,0 +1,68 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {number[]}
+ */
+var getAllElements = function (root1, root2) {
+    let arr = []
+
+    function traverse(root) {
+        if (!root) return;
+
+        arr.push(root.val)
+        traverse(root.left)
+        traverse(root.right)
+    }
+
+    traverse(root1)
+    traverse(root2)
+
+    console.log(arr)
+
+
+    function mearge(arr , f , l , mid){
+        let i = f
+        let j = mid + 1
+        let temp = []
+
+        while(i<=mid && j<=l){
+            if(arr[i] < arr[j]){
+                temp.push(arr[i++])
+            }else{
+                temp.push(arr[j++])
+            }
+        }
+
+        while(i<=mid){
+            temp.push(arr[i++])
+        }
+        while(j<=l){
+            temp.push(arr[j++])
+        }
+
+        for(let c = 0 ; c < temp.length ;c++){
+            arr[f++] = temp[c]
+        }
+    }
+
+    function divide(arr , f , l){
+        if(f>=l) return;
+        let mid = Math.floor((f+l)/2)
+        divide(arr , f , mid)
+        divide(arr , mid+1 , l)
+        mearge(arr , f , l , mid)
+    }
+
+    divide(arr , 0 , arr.length-1)
+
+    return arr
+
+};
