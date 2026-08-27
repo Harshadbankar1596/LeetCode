fb6d27ebce634b04755c8144a9e72a965312b797
Harshad Bankar <harshadbankar1596@gmail.com>
2026-08-27 20:30:22 +0530

    Time: 0 ms (100%), Space: 54.7 MB (86.57%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view-08-27-2026-20-29-58.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+    let temp = []
+
+    function traverse(root, level) {
+        if (!root) return;
+
+
+        if (!temp[level]) temp[level] = 0;
+        temp[level] = root.val
+
+
+        traverse(root.left, level + 1);
+        traverse(root.right, level + 1)
+    }
+
+    traverse(root, 0)
+
+    return temp
+};
