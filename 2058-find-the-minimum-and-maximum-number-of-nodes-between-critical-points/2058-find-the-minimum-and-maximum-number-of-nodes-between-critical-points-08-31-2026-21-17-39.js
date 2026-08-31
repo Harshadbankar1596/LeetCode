26e956786a324187e17a45f7def57525e25eaeae
Harshad Bankar <harshadbankar1596@gmail.com>
2026-08-31 21:18:08 +0530

    Time: 17 ms (50%), Space: 87.9 MB (48.08%) - LeetHub

---
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points-08-31-2026-21-17-39.js
@@ -0,0 +1,68 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+var nodesBetweenCriticalPoints = function (head) {
+    let temp = []
+    let pre = null;
+    let current = head
+    let index = 1
+
+    while (current) {
+        if (current.next && pre && current.val > pre.val && current.next.val < current.val) {
+            temp.push(index)
+        } else if (current.next && pre && current.val < pre.val && current.next.val > current.val) {
+            temp.push(index)
+        }
+        index++
+        pre = current
+        current = current.next
+    }
+    if (!temp.length || temp.length === 1) return [-1, -1]
+
+
+    // let i = 0
+    // let j = 0
+    // let min = +Infinity
+    // let max = -Infinity
+
+    // while (i < temp.length) {
+    //     while(j < temp.length){
+    //         if(i === j){
+    //             j++;
+    //             continue;
+    //         }
+    //         let cur = Math.abs(temp[i] - temp[j])
+    //         if(cur < min){
+    //             min = cur
+    //         }
+    //         if(cur > max){
+    //             max = cur
+    //         }
+    //         j++
+    //     }
+    //     i++
+    //     j = 0
+    // }
+
+    // return [min, max]
+
+    temp.sort((a, b) => a - b)
+
+let min = Infinity
+
+for (let i = 1; i < temp.length; i++) {
+    min = Math.min(min, temp[i] - temp[i - 1])
+}
+
+let max = temp[temp.length - 1] - temp[0]
+
+return [min , max]
+};
