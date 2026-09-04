3038d7ae85373b5da59bbac7bdab85a5b11075ca
Harshad Bankar <harshadbankar1596@gmail.com>
2026-09-04 16:19:27 +0530

    Time: 3 ms (59.26%), Space: 56 MB (85.19%) - LeetHub

---
+++ JavaScript/Easy/3903-smallest-stable-index-i/3903-smallest-stable-index-i-09-04-2026-16-18-54.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var firstStableIndex = function (nums, k) {
+    const n = nums.length;
+
+    const suffixMin = new Array(n);
+
+    suffixMin[n - 1] = nums[n - 1];
+
+    for (let i = n - 2; i >= 0; i--) {
+        suffixMin[i] = Math.min(nums[i], suffixMin[i + 1]);
+    }
+
+    let leftMax = -Infinity;
+
+    for (let i = 0; i < n; i++) {
+        leftMax = Math.max(leftMax, nums[i]);
+
+        const instability = leftMax - suffixMin[i];
+
+        if (instability <= k) {
+            return i;
+        }
+    }
+
+    return -1;
+};
