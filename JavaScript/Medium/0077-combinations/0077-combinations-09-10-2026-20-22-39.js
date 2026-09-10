ef8dc04672588f1fe870491dc070c327a97ff2fd
Harshad Bankar <harshadbankar1596@gmail.com>
2026-09-10 20:23:20 +0530

    Time: 76 ms (23.25%), Space: 128.1 MB (69.04%) - LeetHub

---
+++ JavaScript/Medium/0077-combinations/0077-combinations-09-10-2026-20-22-39.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+    let temp = []
+
+    function track(root , start){
+        if(root.length === k){
+            temp.push([...root])
+        }
+
+        for(let i = start ; i <= n ; i++){
+            root.push(i)
+            track(root , i + 1)
+            root.pop()
+        }
+    }
+
+    track([] , 1)
+    return temp
+};
